f61813f67e6a90b06bf97c31f30afba922df95d6
Luca Cornaglia <lcornag@gmail.com>
2018-04-05 16:01:55 +0200

    dpkg configure en script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
 sudo apt-get update
+
+sudo dpkg --configure -a #IMPORTANTE
+
 sudo apt-get install -y mysql-server php-mysql
 debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
+
 sudo apt-get -y install nodejs
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
