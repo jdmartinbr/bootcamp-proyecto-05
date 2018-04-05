ed00ec0e53763b83dfef5daae4823d49eb823fdf
jdmartinbr <jd.martinbr@gmail.com>
2018-04-05 21:28:22 +0200

    vagrant script fixed

--- script.sh
+++ script.sh
@@ -1,16 +1,8 @@
-#!/usr/bin/env bash
 sudo apt-get update
-
-sudo dpkg --configure -a #IMPORTANTE
-
-sudo apt-get install -y mysql-server php-mysql
 debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
-
+sudo apt-get install -y mysql-server php-mysql
 sudo apt-get -y install nodejs
 sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
 sudo mysql -uroot -pmysql < /vagrant/database/sql.sql
