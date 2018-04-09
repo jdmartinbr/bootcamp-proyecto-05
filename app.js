aede748b7077a1bfe936dc480fed6d5f87149ae1
jdmartinbr <jd.martinbr@gmail.com>
2018-04-09 21:12:55 +0200

    winston logger

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 //app.use(logger('dev'));
-//app.use(logger('combined', {stream: winston.stream}));
+app.use(logger('combined', {stream: winston.stream}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
